805a108f8fe74aed118624ce921df8f8289e1559
liuyubing <k549873769@163.com>
2021-03-30 15:13:42 +0800

    com重定向到net, 可以直接跳转到旧的收藏页面了

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         9dm每日计算自动填入
 // @namespace    http://tampermonkey.net/
-// @version      0.4
-// @description  9dm 每日计算验证自动填入
+// @version      1.0
+// @description  9dm每日计算验证自动填入, com重定向到net, 可以直接跳转到旧的收藏页面了
 // @author       super puffer fish
 // @match        *://www.9dmdamaomod.com/*
 // @match        *://www.9dmdamaomod.net/*
@@ -10,12 +10,18 @@
 // ==/UserScript==
 
 (function () {
-  'use strict';
+  'use strict'
+  const { search, protocol } = window.location
+  if (window.location.pathname === '/gonggao/') {
+    // 如果pathname为/gonggao/则为com到net重定向地址
+    window.open(protocol + '//www.9dmdamaomod.net' + search ? '/forum.php' + search : '', '_self')
+  }
+
   var timeout = setTimeout(() => {
     clearTimeout(timeout)
     if (document.getElementsByName('answer').length) {
-      document.getElementsByName('answer')[0].value = document.querySelector('b').innerText.replace(/[^0-9]/ig,",").split(',').filter(i => Number(i)).reduce((a, b) => Number(a) + Number(b))
+      document.getElementsByName('answer')[0].value = document.querySelector('b').innerText.replace(/[^0-9]/ig, ",").split(',').filter(i => Number(i)).reduce((a, b) => Number(a) + Number(b))
       document.getElementsByName('secqsubmit')[0].click()
     }
   }, 100)
-})();
+})()
