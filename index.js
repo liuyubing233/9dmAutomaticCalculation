111e31cd71ad055dfbeff738a212aad038afb8fd
liuyubing <k549873769@163.com>
2023-03-10 23:33:11 +0800

    fix: 网站地址更新

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         9dm每日计算自动填入
 // @namespace    http://tampermonkey.net/
-// @version      2.3.0
+// @version      2.3.1
 // @description  9dm每日计算验证自动填入；旧的收藏页面域名com重定向到新的net；搜索计算自动验证
 // @author       liuyubing
 // @match        *://www.9dmdamaomod.com/*
 // @match        *://www.9dmdamaomod.net/*
 // @match        *://www.9damaogame.net/*
+// @match        *://www.9damaogames.com/*
 // @grant        none
 // ==/UserScript==
 
@@ -15,7 +16,7 @@
   const { search, protocol, pathname } = window.location;
   if (/\/gonggao\//.test(pathname)) {
     // 如果pathname为/gonggao/则为com到net重定向地址
-    window.open(protocol + "//www.9dmdamaomod.net" + search ? "/forum.php" + search : "", "_self");
+    window.open(protocol + "//www.9damaogames.com" + search ? "/forum.php" + search : "", "_self");
   }
 
   // 每日计算自动填入
