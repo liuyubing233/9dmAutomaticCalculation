046cb6a863ce3283ee70491c268583c76edaf644
liuyubing <k549873769@163.com>
2023-03-20 18:27:40 +0800

    fix: 解决计算失效

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         9dm每日计算自动填入
 // @namespace    http://tampermonkey.net/
-// @version      2.4.0
+// @version      2.4.1
 // @description  9dm每日计算验证自动填入；旧的收藏页面地址重定向到新地址；搜索计算自动验证
 // @author       liuyubing
 // @match        *://www.9dmdamaomod.com/*
@@ -26,20 +26,22 @@
     return;
   }
 
-  // 每日计算自动填入
-  setTimeout(() => {
-    if (document.getElementsByName("answer").length) {
-      document.getElementsByName("answer")[0].value = document
-        .querySelector("b")
-        .innerText.replace(/[^0-9]/gi, ",")
-        .split(",")
-        .filter((i) => Number(i))
-        .reduce((a, b) => Number(a) + Number(b));
-      document.getElementsByName("secqsubmit")[0].click();
-    }
-  }, 100);
+  window.addEventListener("load", () => {
+    // 每日计算自动填入
+    setTimeout(() => {
+      if (document.getElementsByName("answer").length) {
+        document.getElementsByName("answer")[0].value = document
+          .querySelector("b")
+          .innerText.replace(/[^0-9]/gi, ",")
+          .split(",")
+          .filter((i) => Number(i))
+          .reduce((a, b) => Number(a) + Number(b));
+        document.getElementsByName("secqsubmit")[0].click();
+      }
+    }, 100);
 
-  // 搜索计算自动验证
-  const domSearchMask = document.querySelector(".mask");
-  domSearchMask && (domSearchMask.style.display = "none");
+    // 搜索计算自动验证
+    const domSearchMask = document.querySelector(".mask");
+    domSearchMask && (domSearchMask.style.display = "none");
+  });
 })();
