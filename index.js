e2fbd7b59d9048e389e4c4532e337956bfa304c9
liuyubing <k549873769@163.com>
2024-10-28 11:47:04 +0800

    feat: 修改重定向结构，9dm地址结构已更新

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         9dm每日计算自动填入
 // @namespace    http://tampermonkey.net/
-// @version      2.4.4
+// @version      2.4.5
 // @description  9dm每日计算验证自动填入；旧的收藏页面地址重定向到新地址；搜索计算自动验证
 // @author       liuyubing
 // @match        *://www.9dmdamaomod.com/*
@@ -16,35 +16,41 @@
 // ==/UserScript==
 
 (function () {
-  "use strict";
+  'use strict';
   /** 当前使用的 host */
-  const CURRENT_HOST = "www.9dmsgame.net";
+  const CURRENT_HOST = 'www.9dmgamemod.com';
 
   const { search, protocol, pathname, host } = window.location;
   if (/\/gonggao\//.test(pathname) || host !== CURRENT_HOST) {
     // 如果pathname为/gonggao/则为com到net重定向地址
     // 如果当前 host 跟预设 host 不同则重定向
-    const nHref = protocol + "//" + CURRENT_HOST + (search ? "/forum.php" + search : "");
-    window.open(nHref, "_self");
-    return;
+    let tid = '';
+    try {
+      tid = search.match(/tid=(\d+)/)[1];
+    } catch {}
+    if (tid) {
+      const nHref = `${protocol}//${CURRENT_HOST}/thread-${tid}-1-1.html`;
+      window.open(nHref, '_self');
+      return;
+    }
   }
 
-  window.addEventListener("load", () => {
+  window.addEventListener('load', () => {
     // 每日计算自动填入
     setTimeout(() => {
-      if (document.getElementsByName("answer").length) {
-        document.getElementsByName("answer")[0].value = document
-          .querySelector("b")
-          .innerText.replace(/[^0-9]/gi, ",")
-          .split(",")
+      if (document.getElementsByName('answer').length) {
+        document.getElementsByName('answer')[0].value = document
+          .querySelector('b')
+          .innerText.replace(/[^0-9]/gi, ',')
+          .split(',')
           .filter((i) => Number(i))
           .reduce((a, b) => Number(a) + Number(b));
-        document.getElementsByName("secqsubmit")[0].click();
+        document.getElementsByName('secqsubmit')[0].click();
       }
     }, 100);
 
     // 搜索计算自动验证
-    const domSearchMask = document.querySelector(".mask");
-    domSearchMask && (domSearchMask.style.display = "none");
+    const domSearchMask = document.querySelector('.mask');
+    domSearchMask && (domSearchMask.style.display = 'none');
   });
 })();
